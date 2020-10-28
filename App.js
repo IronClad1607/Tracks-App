51795201e773419964e084818fb1d638319f17b9
Ishaan Jain <ishaan160799@gmail.com>
2020-10-28 16:33:48 +0530

    auth action functions created

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import SignupScreen from "./src/screens/SignupScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
+import { Provider as AuthProvider } from "./src/context/AuthContext";
 
 const switchNavigation = createSwitchNavigator({
   loginFlow: createStackNavigator({
@@ -24,5 +25,12 @@ const switchNavigation = createSwitchNavigator({
   }),
 });
 
+const App = createAppContainer(switchNavigation);
 
-export default createAppContainer(switchNavigation)
+export default () => {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+};
