2cf76cd456523f5333ddc6995c08f8732532fa98
Ishaan Jain <ishaan160799@gmail.com>
2020-10-28 13:52:11 +0530

    navigation implemented

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
+import React from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import AccountScreen from "./src/screens/AccountScreen";
 import SigninScreen from "./src/screens/SigninScreen";
-import Signucreen from "./src/screensSignuScreen";
+import SignupScreen from "./src/screens/SignupScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
+
+const switchNavigation = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen,
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      List: TrackListScreen,
+      Detail: TrackDetailScreen,
+    }),
+    Create: TrackCreateScreen,
+    Account: AccountScreen,
+  }),
+});
+
+
+export default createAppContainer(switchNavigation)
