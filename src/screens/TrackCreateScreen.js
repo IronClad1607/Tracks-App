b01d9af21b55692c54471b6f0a59e35150bbeb5d
Ishaan Jain <ishaan160799@gmail.com>
2020-10-30 10:12:25 +0530

    mock location file added

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,3 +1,4 @@
+import "../_mockLocation"
 import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text, Button, Input } from "react-native-elements";
