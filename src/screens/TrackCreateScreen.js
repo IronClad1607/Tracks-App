d959005230a34ff50b78b4b83b3e5914005361a8
Ishaan Jain <ishaan160799@gmail.com>
2020-10-30 08:43:50 +0530

    create screen started

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, Button, Input } from "react-native-elements";
+import { SafeAreaView } from "react-navigation";
+import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
-  return <Text style={{ fontSize: 48 }}>Track Create Screen</Text>;
+  return (
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text h2> Create A Track</Text>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({});
