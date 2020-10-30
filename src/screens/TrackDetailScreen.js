c8b7c78ff34684842fbb2a9489d676de535226ca
Ishaan Jain <ishaan160799@gmail.com>
2020-10-30 20:56:56 +0530

    tracks fetched from api

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
+import { Context as TrackContext } from "../context/TrackContext";
 
-const TrackDetailScreen = () => {
-  return <Text style={{ fontSize: 48 }}>Track Detail Screen</Text>;
+const TrackDetailScreen = ({ navigation }) => {
+  const id = navigation.getParam("id");
+  const { state } = useContext(TrackContext);
+
+  const track = state.find((t) => t._id === id);
+
+return <Text style={{ fontSize: 48 }}>{track.name}</Text>;
 };
 
 const styles = StyleSheet.create({});
